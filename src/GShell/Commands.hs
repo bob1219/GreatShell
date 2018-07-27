4734066f64d14c9b0ee22f14f34613e3552db3bb
吉田大輝 <bob181219@yahoo.co.jp>
2018-07-27 14:58:29 +0900

    Describe command_cpfile function in GShell.Commands module

--- src/GShell/Commands.hs
+++ src/GShell/Commands.hs
@@ -20,7 +20,7 @@ module GShell.Commands (commandProcess, tokenizeCommand) where
 import System.IO	(hPutStrLn, stderr)
 import System.IO.Error	(catchIOError)
 import GShell.Constants	(unknownException)
-import System.Directory	(removeFile)
+import System.Directory	(removeFile, copyFileWithMetadata)
 
 commandProcess :: [String] -> IO ()
 commandProcess []		=	error "got empty list"
@@ -60,6 +60,15 @@ command_rmfile :: String -> IO ()
 command_rmfile filename =	(removeFile filename)
 					`catchIOError` (\e -> case e of _	| isDoesNotExistError e	-> commandLineError "that file does not exist"
 										| isAlreadyInUseError e	-> commandLineError "that file already in use"
+										| isIllegalOperation e	-> commandLineError "that operation is illegal"
+										| isPermissionError e	-> commandLineError "you do not have the permission"
+										| otherwise		-> unknownException e)
+
+command_cpfile :: String -> String -> IO ()
+command_cpfile src dst =	(copyFileWithMetadata src dst)
+					`catchIOError` (\e -> case e of _	| isDoesNotExistError e	-> commandLineError "that source-file does not exist"
+										| isAlreadyInUseError e	-> commandLineError "that files already in use"
+										| isFullError e		-> commandLineError "your device is full"
 										| isIllegalOperation e	-> commandLineError "that operation is illegal"
 										| isPermissionError e	-> commandLineError "you do not have the permission"
 										| otherwise		-> unknownException e)
